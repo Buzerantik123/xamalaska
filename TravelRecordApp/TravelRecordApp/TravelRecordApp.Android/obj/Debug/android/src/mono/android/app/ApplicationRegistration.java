588638d79ccd2e1eaab27bd2571239355145b357
Matus Lang <marcel.patrk@gmail.com>
2018-02-14 21:30:06 +0100

    pushing the whole project

---
+++ TravelRecordApp/TravelRecordApp/TravelRecordApp.Android/obj/Debug/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,11 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("TravelRecordApp.Droid.MainApplication, TravelRecordApp.Android, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", md5186acefcb006d1f2f2d0974a9ae8757b.MainApplication.class, md5186acefcb006d1f2f2d0974a9ae8757b.MainApplication.__md_methods);
+		
+	}
+}
